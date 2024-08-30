90896285e439b66330adc7823a6e6eeeee1d2837
TheCarlosTovias <rosales.carlost@gmail.com>
2024-08-30 16:27:04 -0500

    Added necessary packages, and login component

---
+++ frontend/fitness-web-app/src/components/authentication/Login.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+        <h1>Login</h1>
+        <label name="email">
+            Email ID <input></input>
+        </label>
+        <label name="password"> 
+            Password <input></input>
+        </label>
+        </div>
+    )
+}
+
+export default Login
+
