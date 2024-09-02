400e7a454212a192822e7aa20746a097f92f56b7
TheCarlosTovias <rosales.carlost@gmail.com>
2024-09-02 17:03:05 -0500

    styled login page

--- frontend/fitness-web-app/src/components/authentication/Login.tsx
+++ frontend/fitness-web-app/src/components/authentication/Login.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
+import rectangleImage from './Rectangle.png';
+import rectangleImage1 from './Rectangle1.png';
 
 import styles from './Login.module.css';
 
 const Login = () => {
     return (
         <div className={styles.flex}>
-        <h1>Login</h1>
-        <label name="email">
-            Email ID <input></input>
+            <img src={rectangleImage} alt="rectangle-image" className={styles.cornerImage}/>
+            <img src={rectangleImage1} alt="rectangle-image1" className={styles.cornerImage1}/>
+        <h2 className={styles.title}>Login</h2>
+        <label className={styles.forms} name="email">
+            Email ID <input className={styles.input}></input>
         </label>
-        <label name="password"> 
-            Password <input></input>
+        <label className={styles.forms} name="password"> 
+            Password <input className={styles.input}></input>
         </label>
-        <button>Login</button>
-        <p>Forgot password?</p>
-        <p>Don't have an account? Create new</p>
-        <p>or login with</p>
-        <button>Sign in with Google</button>
+        <div className={styles.divRow}>
+        <button className={styles.loginBtn}>Login</button>
+        <p className={styles.password}>Forgot password?</p>
+        </div>
+        <p className={styles.account}>Don't have an account? <a className={styles.createNew}href="#">Create new</a></p>
+        <div className={styles.loginWithContainer}><p className={styles.loginWith}>Or login with</p></div>
+        <button className={styles.googleBtn}>Sign in with Google</button>
         </div>
     )
 }
