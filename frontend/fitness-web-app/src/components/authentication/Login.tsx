95c05a55e35466364319289222117e767cfc259b
TheCarlosTovias <rosales.carlost@gmail.com>
2024-09-04 19:04:47 -0500

    added services folder for api modularity

--- frontend/fitness-web-app/src/components/authentication/Login.tsx
+++ frontend/fitness-web-app/src/components/authentication/Login.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import rectangleImage from './Rectangle.png';
 import rectangleImage1 from './Rectangle1.png';
 
 import styles from './Login.module.css';
 
 const Login = () => {
+    const [emailInput, setEmailInput] = useState('');
+    const [passwordInput, setPasswordInput] = useState('');
+
+
+    const handleEmailChange = (event) => {
+        setEmailInput(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPasswordInput(event.target.value);
+    };
+
+    const submitUserInformation = () => {
+        const response = await fetch
+    };
+
     return (
         <div className={styles.flex}>
             <img src={rectangleImage} alt="rectangle-image" className={styles.cornerImage}/>
             <img src={rectangleImage1} alt="rectangle-image1" className={styles.cornerImage1}/>
         <h2 className={styles.title}>Login</h2>
         <label className={styles.forms} name="email">
-            Email ID <input className={styles.input}></input>
+            Email ID <input value={emailInput} onChange={handleEmailChange}className={styles.input}></input>
         </label>
         <label className={styles.forms} name="password"> 
-            Password <input className={styles.input}></input>
+            Password <input value={passwordInput} onChange={handlePasswordChange} className={styles.input}></input>
         </label>
         <div className={styles.divRow}>
         <button className={styles.loginBtn}>Login</button>
@@ -22,7 +38,9 @@ const Login = () => {
         </div>
         <p className={styles.account}>Don't have an account? <a className={styles.createNew}href="#">Create new</a></p>
         <div className={styles.loginWithContainer}><p className={styles.loginWith}>Or login with</p></div>
-        <button className={styles.googleBtn}>Sign in with Google</button>
+        <button onClick={submitUserInformation}className={styles.googleBtn}>Sign in with Google</button>
+
+        <p>{emailInput}     {passwordInput}</p>
         </div>
     )
 }
