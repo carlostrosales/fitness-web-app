541f14852d21051afe6d091c2abdc44a8f701421
TheCarlosTovias <rosales.carlost@gmail.com>
2024-09-19 17:22:01 -0500

    added route to workouts apge from home page

--- frontend/fitness-web-app/src/components/App.tsx
+++ frontend/fitness-web-app/src/components/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from '../components/authentication/Login.tsx';
 import Home from '../components/Home.tsx';
+import {Workouts} from '../components/Workouts.tsx';
 
 export const App = () => {
     return (
@@ -8,6 +9,7 @@ export const App = () => {
         <Routes>
             <Route path="/" element={<Login />}/>
             <Route path="/home" element={<Home />}/>
+            <Route path="/workout" element={<Workouts />}/>
         </Routes>
         </>
     );
--- frontend/fitness-web-app/src/components/Home.tsx
+++ frontend/fitness-web-app/src/components/Home.tsx
@@ -1,7 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+
 const Home = () => {
+
+    const navigate = useNavigate();
+
+    const handleChange = async (event: any) => {
+
+        event.preventDefault();
+
+        navigate('/workout');
+        
+    }
+    
     return (
         <div>
             <p>Home Page</p>
+            <button onClick={handleChange}></button>
         </div>
     );
 }
